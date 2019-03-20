45514d8cb23533b4166bfa014002b99b484d6174
LanceKnight <lanceknight26@gmail.com>
2019-03-20 17:05:16 -0500

    create Ortho.* framework

---
+++ Ortho.hh
@@ -0,0 +1,26 @@
+#ifndef ORTHO_HH
+#define ORTHO_HH
+
+#include <string>
+#include <vector>
+
+
+
+class Ortho{
+
+public:
+	Ortho();
+
+	~Ortho();
+
+	std::string static ortho(std::vector<std::string> paramList);
+
+
+	//data
+
+	
+
+};
+
+
+#endif
