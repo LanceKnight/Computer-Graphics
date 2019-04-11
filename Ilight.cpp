fd6687860f1f7add4bd8977d0b2dfbdd0cd65e81
LanceKnight <lanceknight26@gmail.com>
2019-04-11 16:49:22 -0500

    Ilight works

--- Ilight.cpp
+++ Ilight.cpp
@@ -1,25 +1,54 @@
+#include <cstring>
 #include <iostream>
 #include "Ilight.hh"
-#include "3D.h"
+#include "Trace.hh"
 #include "Util.hh"
+
+int Ilight::counter_=0;
+
+Ilight::Ilight(){
+
+}
+
+Ilight::Ilight(float lr, float lg, float lb,
+		   	   float dx, float dy, float dz){
+	lr_ = lr;
+	lg_ = lg;
+	lb_ = lb;
+	dx_ = dx;
+	dy_ = dy;
+	dz_ = dz;
+	name_ = "light "+std::to_string(counter_);
+	counter_++;
+
+}
+
+Ilight::~Ilight(){
+
+}
 
 
 std::string
 Ilight::ilight(std::vector<std::string> paramList){
 
 
-	if(paramList.size()<=3){
-		if(paramList.size() <3){
+	if(paramList.size()<=6){
+		if(paramList.size() <6){
 				return "too few parameters";
 			}
-			float x = 0;
-			float y = 0;
-			float z = 0;
-
+			float lr = 0;
+			float lg = 0;
+			float lb = 0;
+			float dx = 0;
+			float dy = 0;
+			float dz = 0;
 			try{
-				x = std::stod(paramList[0]);
-				y = std::stod(paramList[1]);
-				z = std::stod(paramList[2]);
+				lr = std::stod(paramList[0]);
+				lg = std::stod(paramList[1]);
+				lb = std::stod(paramList[2]);
+				dx = std::stod(paramList[3]);
+				dy = std::stod(paramList[4]);
+				dz = std::stod(paramList[5]);
 
 			}
 			catch(...){
@@ -27,18 +56,9 @@ Ilight::ilight(std::vector<std::string> paramList){
 
 			}
 
+			Ilight * new_light = new Ilight(lr, lg, lb, dx, dy, dz);
 
-
-			matrix_unit * ilight_mat = new matrix_unit({
-					   { {1., 0., 0., x},
-					     {0., 1., 0., y},
-					     {0., 0., 1., z},
-					     {0., 0., 0., 1.0}  },
-					});
-			matrix_unit tmpsln;
-
-			Mult_mat(stack[top], ilight_mat, &tmpsln);
-			Copy_mat(&tmpsln, stack[top]);
+			Trace::light_list_.push_back(new_light);
 
 		return "Ilight Done";
 	}
--- Ilight.hh
+++ Ilight.hh
@@ -11,13 +11,18 @@ class Ilight{
 public:
 	Ilight();
 
+	Ilight(float lr, float lg, float lb,
+		   float dx, float dy, float dz);
+
 	~Ilight();
 
 	std::string static ilight(std::vector<std::string> paramList);
 
 
 	//data
-
+	float lr_, lg_, lb_, dx_, dy_, dz_;
+	std::string name_;
+	static int counter_;
 	
 
 };
