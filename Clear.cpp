b2b31188851d8ef30f08ba3f6e81c29753a0fb63
LanceKnight <lanceknight26@gmail.com>
2019-04-11 16:48:42 -0500

    fix bug of not setting object_list, light_light, counters to default for Clear

--- Clear.cpp
+++ Clear.cpp
@@ -2,8 +2,11 @@
 
 #include "Background.hh"
 #include "Clear.hh"
+#include "Ilight.hh"
 #include "Screen.hh"
+#include "Sphere.hh"
 #include "Trace.hh"
+#include "Triangle.hh"
 
 #include "main.hh"
 #include "Util.hh"
@@ -26,8 +29,20 @@ Clear::clear(std::vector<std::string> paramList){
 		Background::r_ =0;
 		Background::g_ =0;
 		Background::b_ =0;
-		Trace::object_list_.clear();
+		for(std::vector<Shape*>::iterator it = Trace::object_list_.begin();it != Trace::object_list_.end();++it){
+			delete *it;
+		}
+		for(std::vector<Ilight *>::iterator light_it = Trace::light_list_.begin();light_it != Trace::light_list_.end();++light_it){
+			delete *light_it;
+		}
 
+
+		Trace::object_list_.clear();
+		Trace::light_list_.clear();
+
+		Triangle::counter_=0;
+		Sphere::counter_=0;
+		Ilight::counter_=0;
 		return "Clear Done";
 	}
 	else{
