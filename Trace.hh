a059bab05ddd0a107a33ec91bb3a8a2abdffb5c1
LanceKnight <lanceknight26@gmail.com>
2019-04-18 16:39:58 -0500

    deleted intersectSquare()

--- Trace.hh
+++ Trace.hh
@@ -22,17 +22,21 @@ public:
 	static std::vector<Ilight *> light_list_;
 
 private:
+	static vec3 getColorFromRay(Ray ray, int i, int j);
+
 	static Ray buildCameraRay(int i,int j);
 
 	static Ray buildReflectedRay(Ray ray, vec3 norm, vec3 point, int i ,int j);
 
-	static vec3 getColorFromRay(Ray ray, int i, int j);
-
 	static float intersect(Ray ray, Shape* shape, int i, int j);
 
-	static float intersectSquare( vec3 vec3_e, vec3 vec3_d, float ax, float xy, float az, float bx, float by, float bz);
+	static float intersectTriangle(Shape* shape, Ray ray);
 
-	static float intersectPlane( vec3 vec3_e, vec3 vec3_d, float nx, float ny, float nz, float px, float py, float pz);
+	static float intersectSphere(Shape* shape, Ray ray);
+
+	static float intersectBox(Shape* shape, Ray ray, int i, int j);
+
+	static float intersectPlane( Shape* shape, Ray ray);
 
 	//private data
 
