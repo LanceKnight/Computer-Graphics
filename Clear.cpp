828fed22c402ad3fb817ae2f827f82e5d6bae881
LanceKnight <lanceknight26@gmail.com>
2019-04-19 17:02:42 -0500

    Shape refactored to Surface

--- Clear.cpp
+++ Clear.cpp
@@ -29,7 +29,7 @@ Clear::clear(std::vector<std::string> paramList){
 		Background::r_ =0;
 		Background::g_ =0;
 		Background::b_ =0;
-		for(std::vector<Shape*>::iterator it = Trace::object_list_.begin();it != Trace::object_list_.end();++it){
+		for(std::vector<Surface*>::iterator it = Trace::object_list_.begin();it != Trace::object_list_.end();++it){
 			delete *it;
 		}
 		for(std::vector<Ilight *>::iterator light_it = Trace::light_list_.begin();light_it != Trace::light_list_.end();++light_it){
